b44d98a763724353fc1cb091f5ce37a5dfd09ef9
Adi Siswanto <adi@atomic.id>
2024-06-27 16:04:59 +0700

    koneksi

--- production/koneksi.php
+++ production/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "beasiswa_wp");
+$koneksi = mysqli_connect("localhost", "adi", "adi15siswanto", "water_wp");
 
 if ($koneksi->connect_errno) {
     echo "Failed to connect to MySQL: " . $koneksi->connect_error;
